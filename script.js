87713b67b0ea0d5e7f87d82a6b92d856dfca833d
justincbal <justinseebee@hotmail.com>
2022-09-19 12:26:19 -0400

    finish buttons

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const drawingArea = document.querySelector('.drawing-area');
-const size = window.prompt("Enter amount", "1-64");
+const start = document.querySelector('.start-btn');
+const reset = document.querySelector('.reset-btn');
+let size;
 
+function createGrid(sz) {
 
-
-function createGrid() {
-    for(let i = 0; i < size; i++) {
+    // Creates new grid
+    for(let i = 0; i < sz; i++) {
         // Create columns
         const gridCol = document.createElement('div');
         gridCol.classList.add('grid');
-        for(let j = 0; j < size; j++) {
+        for(let j = 0; j < sz; j++) {
+            // Create rows
             const gridRow = document.createElement('div')
             gridRow.classList.add('grid2');
             gridCol.append(gridRow);
@@ -17,4 +20,41 @@ function createGrid() {
     }
 }
 
-createGrid()
+// Start button
+start.addEventListener('click', () =>{
+    resizeGrid();
+    size = window.prompt("Enter amount", "1-64");
+    while (size < 1 || size > 64) {
+        size = window.prompt("Invalid size, must be 1-64:", "1-64");
+    }
+    createGrid(size);
+
+    // Handles hover effect
+    const grid = document.querySelectorAll('.drawing-area .grid .grid2');
+    console.log(grid.length);
+    grid.forEach((tile) => {
+    tile.addEventListener('mouseover', () => {
+        tile.classList.add('test');
+    })
+})
+})
+
+function resizeGrid() {
+    for(let i = 0; i < size; i++) {
+        const column = document.querySelectorAll('.grid');
+        column.forEach(col => {col.remove();})
+    }
+}
+
+
+// Reset button
+reset.addEventListener('click', () => {
+    // Creates new grid
+    const grids = document.querySelectorAll('.grid2');
+    grids.forEach((grid) => {
+        grid.classList.remove('test');
+    })
+})
+
+
+
